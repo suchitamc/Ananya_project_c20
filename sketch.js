33a36730f4f5ee177289bc28d57652b9a6c9552f
Suchita Chatterjee <76135810+suchitamc@users.noreply.github.com>
2021-01-11 19:26:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var jerry, jerryewalking, jerrystanding, jerryresting;
 function preload() {
     
   tomresting = loadAnimation("tomOne.png");
-  tomwalking = loadAnimation("tomTwo.png","images/tomthree.png");
+  tomwalking = loadAnimation("tomTwo.png","tomthree.png");
   tomstanding = loadAnimation("tomFour.png");
   gardenImg = loadImage("garden.png");
 
